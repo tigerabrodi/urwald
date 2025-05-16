6e30156b83b8bb24942a9f8b7a95caa252dd94df
Tiger Abrodi <tigerabrodi@gmail.com>
2025-05-16 20:04:15 +0200

    test to be for new implementation of render list

--- src/tests/utils.test.ts
+++ src/tests/utils.test.ts
@@ -1,4 +1,5 @@
 import { el } from "../el";
+import { state } from "../state";
 import { renderList } from "../utils";
 
 describe("renderList", () => {
@@ -20,13 +21,14 @@ describe("renderList", () => {
     document.body.appendChild(container);
 
     // Render the list
-    const fragment = renderList(
-      () => items,
-      (item) =>
-        el("li").text(item.text).attr({ "data-id": item.id.toString() }).done()
-    );
-
-    container.appendChild(fragment);
+    renderList({
+      container,
+      state: state({ items }),
+      getItems: (state) => state.items,
+      itemRenderer: (item) =>
+        el("li").text(item.text).attr({ "data-id": item.id.toString() }).done(),
+      getItemKey: (item) => item.id,
+    });
 
     // Check that the list was rendered correctly
     const listItems = container.querySelectorAll("li");
@@ -35,28 +37,5 @@ describe("renderList", () => {
     expect(listItems[1].textContent).toBe("Item 2");
     expect(listItems[2].textContent).toBe("Item 3");
     expect(listItems[0].getAttribute("data-id")).toBe("1");
-  });
-
-  // This test is just a placeholder since our current implementation
-  // doesn't handle reactivity yet
-  test("should update when source array changes", () => {
-    // Note: This would require integration with your state management
-    // For now, we're just checking the basic functionality
-    const items = [{ id: 1, text: "Item 1" }];
-
-    const getItems = () => items;
-
-    const container = document.createElement("ul");
-    document.body.appendChild(container);
-
-    const fragment = renderList(getItems, (item) =>
-      el("li").text(item.text).done()
-    );
-
-    container.appendChild(fragment);
-
-    // Verification of initial state
-    expect(container.querySelectorAll("li").length).toBe(1);
-    expect(container.querySelector("li")?.textContent).toBe("Item 1");
   });
 });
