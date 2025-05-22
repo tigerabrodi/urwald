bf9ffd8a7626ce92f9833af9199546985bff2329
Tiger Abrodi <tigerabrodi@gmail.com>
2025-05-22 07:29:33 +0200

    clarify folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-// Export all public API components
-export { el } from "./el";
-export { link, RouteDefinition, Router, router } from "./router";
-export { state, StateManager } from "./state";
-export {
-  computed,
-  debounce,
-  isDeepEqual,
-  renderList,
-  throttle,
-  toggleClass,
-  when,
-} from "./utils";
+// it is ok to export everything from the submodules
+// internal modules are under /src/internal
+export * from "./el";
+export * from "./router";
+export * from "./state";
+export * from "./utils";
